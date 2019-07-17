18a6633737027336dca3b9609eb681817b198b41
alexLang <l738367@tom.com>
2019-07-17 08:29:03 +0800

    加入swiper在脚手架使用

--- 脚手架极速之路-项目.js
+++ 脚手架极速之路-项目.js
@@ -1,9 +1,9 @@
 /**
  * 快速迭代脚手架vue-cli:
- *          2.x                                                                             3.x
+ *       2.x ▁  3.x ●
  * 
- *      ▁ 配置自动打开页面 (./config/index.js-autoOpenBrowser)                                ——
- *      ▁ 配置exlintrc检测 (./eslintrc.js)                                                   ——
+ *      ▁ 配置自动打开页面 (./config/index.js-autoOpenBrowser)
+ *      ▁ 配置exlintrc检测 (./eslintrc.js)
  * 
  * 
  *      ● HTML --- ①meta适配 ②样式重置 ③解决移动端0.3s点击延迟, fastclick库
@@ -11,8 +11,8 @@
  * 
  *      ● 建文件夹( api、router路由器、store状态管理、common混合、components、pages路由组件、filter过滤器、mock模拟后台数据)
  *      ● 下包
- *          stylus              ---   npm install stylus stylus-loader、
- *          路由、ajax           ---   npm install vue-router axios、
+ *          stylus              ---   npm install stylus stylus-loader
+ *          路由、ajax           ---   npm install vue-router axios
  *          vuex、swiper、dayjs  ---   npm install vuex swiper dayjs
  *          Mint UI(按需加载)    ---    npm i mint-ui -S   **  npm install babel-plugin-component -D
  * 
@@ -54,6 +54,19 @@
  *          }
  *        *** config/index.js***
  *        const prefix = process.env.NODE_ENV === "development" ? "" : "http://localhost:5000"
+ * 
+ * ------------------------------------------------------------------------------------------------------------------------------------
+ * 
+ * swiper轮播图: 
+ * 
+ *  import Swiper from "swiper";  // 导入Swiper
+ * 
+ *  var mySwiper = new Swiper(".swiper-container", {  // 实例化的方式使用(页面加载完成之后、异步数据更新DOM加载完后)
+ *     loop: true, // 循环模式选项                              | 配合**vm.$nextTick(callback)使用** --- 在下次 DOM 更新循环结束之后执行延迟回调。                        
+ *     pagination: {                                                    | watch+vm.$nextTick(callback)                                                
+ *       el: ".swiper-pagination"                                       | this.$store.dispatch('actions',() => {vm.$nextTick(callback)} )             
+ *     }                                                                | await this.$store.dispatch('actions') 后, 再写 this.$nextTick(callback)         
+ *   });
  * 
  * 
  * 
